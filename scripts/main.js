bca8f48cc201979ed13175010ea41ea48d2394f0
suzie33 <valiazin@gmail.com>
2020-03-19 22:11:57 +0300

    added hamburger menu functionality

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('#hamburger-menu-link');
+const nav = document.querySelector('#main-nav');
+const close = document.querySelector('#close-nav-button');
+
+hamburger.addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    nav.classList.add('nav--hamburger');
+})
+
+close.addEventListener ('click', function(event) {
+    event.preventDefault();
+
+    nav.classList.remove('nav--hamburger');
+})
