be215fddddb6700f0a52c9f8daa99445f811a5e0
suzie33 <valiazin@gmail.com>
2020-03-20 16:52:15 +0300

    burgers section markup and stylization

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,10 @@ hamburger.addEventListener('click', function (event) {
     event.preventDefault();
     
     nav.classList.add('nav--hamburger');
-})
+});
 
 close.addEventListener ('click', function(event) {
     event.preventDefault();
 
     nav.classList.remove('nav--hamburger');
-})
+});
