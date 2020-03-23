3a69a3d26f41631bd32aaaf40faf5783e56ffa8e
suzie33 <valiazin@gmail.com>
2020-03-23 23:50:21 +0300

    burgers section stylization

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,36 @@
+/////////////////////////full screen menu functionality///////////////////////
+
 const hamburger = document.querySelector('#hamburger-menu-link');
 const nav = document.querySelector('#main-nav');
-const close = document.querySelector('#close-nav-button');
+const closeNavButton = document.querySelector('#close-nav-button');
 
 hamburger.addEventListener('click', function (event) {
     event.preventDefault();
     
     nav.classList.add('nav--hamburger');
+    document.body.style.overflow = 'hidden';
 });
 
-close.addEventListener ('click', function(event) {
+closeNavButton.addEventListener ('click', function(event) {
     event.preventDefault();
 
     nav.classList.remove('nav--hamburger');
+    document.body.style.overflow = 'visible';
+});
+
+//////////////////////////composition dropout///////////////////////////////////////
+
+const compositionIcon = document.querySelector('#burgers__composition');
+const dropout = document.querySelector('#burgers__dropout');
+const closeDropoutButton = document.querySelector('#close-dropout-button');
+
+compositionIcon.addEventListener('click', function() {
+    
+    dropout.classList.add('burgers__dropout--visible');
+});
+
+closeDropoutButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    dropout.classList.remove('burgers__dropout--visible');
 });
