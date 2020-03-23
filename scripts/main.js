e24c3a12dde27713985b3bafcac33fd36f3d124e
suzie33 <valiazin@gmail.com>
2020-03-24 00:04:52 +0300

    js adjustments

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 const hamburger = document.querySelector('#hamburger-menu-link');
 const nav = document.querySelector('#main-nav');
 const closeNavButton = document.querySelector('#close-nav-button');
+const navItems = document.querySelectorAll('.nav__item');
 
 hamburger.addEventListener('click', function (event) {
     event.preventDefault();
@@ -18,6 +19,12 @@ closeNavButton.addEventListener ('click', function(event) {
     document.body.style.overflow = 'visible';
 });
 
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', function() {
+        document.body.style.overflow = 'visible';
+    });
+}
+
 //////////////////////////composition dropout///////////////////////////////////////
 
 const compositionIcon = document.querySelector('#burgers__composition');
@@ -25,7 +32,7 @@ const dropout = document.querySelector('#burgers__dropout');
 const closeDropoutButton = document.querySelector('#close-dropout-button');
 
 compositionIcon.addEventListener('click', function() {
-    
+
     dropout.classList.add('burgers__dropout--visible');
 });
 
