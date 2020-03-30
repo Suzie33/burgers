d7a3ded2bfcb948e4b621ca48489821a85b991c8
suzie33 <valiazin@gmail.com>
2020-03-30 18:40:53 +0300

    adjustments

--- scripts/main.js
+++ scripts/main.js
@@ -145,7 +145,7 @@ sendButton.addEventListener('click', function(event) {
 
         const xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
-        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail/');
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail/fail');
         xhr.send(formData);
         xhr.addEventListener('load', function() {
             orderPopupText.textContent = xhr.response.message;
