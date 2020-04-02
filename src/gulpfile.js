90780daf493374d4d21044aecffb93cfd4be366d
suzie33 <valiazin@gmail.com>
2020-04-03 01:14:40 +0300

    gulp styles added

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,14 +1,26 @@
-const { src, dest, task } = require("gulp");
+const { src, dest, task, series } = require("gulp");
 const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 task( 'clean', () => {
     return src( '../dist/**/*', { read: false })
       .pipe( rm() )
   })
 
-function copy() {
+ task( 'copy', () => {
     return src('css/**/*.scss').pipe(dest('../dist'));
- }
+ })
 
- exports.copy = copy;
+ const styles = [
+    'node_modules/normalize.css/normalize.css',
+    'css/layout/main.scss'
+   ];
 
+ task('styles', () => {
+    return src(styles)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(dest('../dist'));
+  });
+
+ task('default', series('clean', 'styles'))
