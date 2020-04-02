084d64ec1a290df4b627a8528af449c2448c8f99
suzie33 <valiazin@gmail.com>
2020-04-03 02:16:57 +0300

    browsync installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,44 @@
-const { src, dest, task, series } = require("gulp");
+const { src, dest, task, series, watch } = require("gulp");
 const rm = require('gulp-rm');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 task( 'clean', () => {
     return src( 'dist/**/*', { read: false })
       .pipe( rm() )
-  })
+})
 
- task( 'copy', () => {
-    return src('src/css/**/*.scss').pipe(dest('dist'));
- })
+task('copy:html', () => {
+   return src('src/*.html')
+     .pipe(dest('dist'))
+     .pipe(reload({ stream: true }));
+})
 
- const styles = [
+const styles = [
     'node_modules/normalize.css/normalize.css',
     'src/css/layout/main.scss'
-   ];
+];
 
- task('styles', () => {
+task('styles', () => {
     return src(styles)
+      .pipe(concat('main.scss'))
       .pipe(sass().on('error', sass.logError))
       .pipe(dest('dist'));
-  });
+});
 
- task('default', series('clean', 'styles'))
+task('server', () => {
+   browserSync.init({
+      server: {
+            baseDir: "./dist"
+      },
+      open: false
+  });
+ });
+
+watch('src/styles/**/*.scss', series('styles'));
+watch('src/*.html', series('copy:html'));
+
+task('default', series('clean', 'copy:html', 'styles', 'server'))
