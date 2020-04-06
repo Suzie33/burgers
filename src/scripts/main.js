be70bec2bf87598eb8f56d8fb718dc2a49d475d1
suzie33 <valiazin@gmail.com>
2020-04-06 17:41:29 +0300

    map functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -536,5 +536,39 @@ function onYouTubeIframeAPIReady() {
 
 eventsInit();
 
+////////// map ////////////
 
+let myMap;
 
+const init = () => {
+    myMap = new ymaps.Map("map", {
+        center: [59.935274, 30.312388],
+        zoom: 11,
+        controls: ['zoomControl']
+    });
+
+    const coords = [
+        [59.975088, 30.314779],
+        [59.947556, 30.387081], 
+        [59.893487, 30.317235],
+        [59.918016, 30.511916]
+    ];
+
+    const myCollection = new ymaps.GeoObjectCollection({}, {
+        draggable: false,
+        iconLayout: 'default#image',
+        iconImageHref: 'img/icons/map-marker.svg',
+        iconImageSize: [46, 57],
+        iconImageOffset: [-35, -52]
+    });
+
+    coords.forEach(coord => {
+        myCollection.add(new ymaps.Placemark(coord));
+    });
+
+    myMap.geoObjects.add(myCollection);
+
+    myMap.behaviors.disable('scrollZoom');
+}
+
+ymaps.ready(init);
